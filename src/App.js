19ab266325a63c917d6992c35f64567f2d5805d8
quynhdv <dovanquynh96@gmail.com>
2020-07-23 22:19:47 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './Job'
+import './App.css'
+import JobPagination from './JobPagination';
+import SearchForm from './SearchForm'
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className="my-4">
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+
+      {loading && <div className="lds-hourglass m-auto"></div>}
+
+      {error && <h1>Error. Try again.</h1>}
+
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+
+    </Container>
+  )
 }
 
 export default App;
